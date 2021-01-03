a2bb236e09f953ab228fd7bc8b84911ca5c21d1a
timcheng98 <timchengy@gmail.com>
2021-01-04 00:56:37 +0800

    update

--- truffle-config.js
+++ truffle-config.js
@@ -7,13 +7,9 @@ module.exports = {
       host: "172.16.210.165",
       port: 7545,
       network_id: "*", // Match any network id
-      websockets: true
-    },
-    live: {
-      host: "172.16.210.165",
-      port: 7545,
-      network_id: 1
-  
+      websockets: true,
+      gas: 4698712,
+      gasPrice: 25000000000
     }
   },
 
